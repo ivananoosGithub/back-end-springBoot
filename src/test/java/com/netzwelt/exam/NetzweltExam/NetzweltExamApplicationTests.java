e1748f5d8c013cfadce1f5441be25e081783f578
Ivan Kim Anoos <55166879+ivananoosGithub@users.noreply.github.com>
2023-07-10 21:56:02 +0800

    done setting backend

---
+++ src/test/java/com/netzwelt/exam/NetzweltExam/NetzweltExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netzwelt.exam.NetzweltExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetzweltExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
