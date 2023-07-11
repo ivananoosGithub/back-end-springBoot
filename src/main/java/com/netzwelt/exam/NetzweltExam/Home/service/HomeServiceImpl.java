62879df2aa6c2a4189a42249e69abb5c27e6ae46
Ivan Kim Anoos <55166879+ivananoosGithub@users.noreply.github.com>
2023-07-11 16:02:18 +0800

    Modified pom.xml, added spring security and home

---
+++ src/main/java/com/netzwelt/exam/NetzweltExam/Home/service/HomeServiceImpl.java
@@ -0,0 +1,5 @@
+package com.netzwelt.exam.NetzweltExam.Home.service;
+
+public class HomeServiceImpl {
+
+}
---
+++ src/main/java/com/netzwelt/exam/NetzweltExam/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.netzwelt.exam.NetzweltExam.SecurityConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/home").authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/account/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+}
+
